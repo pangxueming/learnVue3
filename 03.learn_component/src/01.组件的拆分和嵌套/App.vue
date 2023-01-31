dbab0f8352f1cc107da072aef3669f8ddf1d1228
pangxueming <1492678698@qq.com>
2023-02-01 00:26:34 +0800

    01.组件的拆分和嵌套

---
+++ 03.learn_component/src/01.组件的拆分和嵌套/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Header />
+  <Main />
+  <Footer />
+</template>
+
+<script>
+import Header from './Header.vue';
+import Main from './Main.vue';
+import Footer from './Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 03.learn_component/src/01.组件的拆分和嵌套/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main">
+    <main-banner />
+    <main-product-list /> 
+  </div>
+</template>
+
+<script>
+import MainBanner from './MainBanner.vue';
+import MainProductList from './MainProductList.vue';
+
+export default {
+  name: 'Main',
+  components: {
+    MainBanner,
+    MainProductList
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
