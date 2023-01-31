9e13271d671c2880901de428f3c1f7e55b66cbbc
pangxueming <1492678698@qq.com>
2023-02-01 00:26:54 +0800

    组件的css作用域

---
+++ 03.learn_component/src/02.组件的css作用域/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- 通过添加一个div元素包裹解决 -->
+  <div>
+    <h2>app component's text color is blue</h2>
+
+    <hello-world />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  color: blue;
+}
+</style>
---
+++ 03.learn_component/src/02.组件的css作用域/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>hello world component's text color is red</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style scoped>
+h2 {
+  color: red;
+}
+</style>
