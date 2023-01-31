4b69266b0b989bb5207c5ab5d00a61354d3a1c15
pangxueming <1492678698@qq.com>
2023-02-01 01:19:01 +0800

    子组件传递父组件

---
+++ 03.learn_component/src/04.子组件传递父组件/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>当前计数： {{ counter }}</h2>
+
+    <counter-operation-cpn @handle-click-btn="handleChangeCounter" />
+  </div>
+</template>
+
+<script>
+import CounterOperationCpn from "./CounterOperation.vue";
+
+export default {
+  components: {
+    CounterOperationCpn,
+  },
+
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+
+  methods: {
+    handleChangeCounter(count) {
+      this.counter += count;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ 03.learn_component/src/04.子组件传递父组件/CounterOperation.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <button @click="decrement" class="mr-10">-1</button>
+    <button @click="increment">+1</button>
+    <div>
+      <input type="text" v-model.number="addCounter" />
+      <button @click="handleClickAddCounter">+{{ addCounter }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // emits: ["handleClickBtn"], // 显式地声明触发的事件名称
+  emits: {
+    // 对象写法的目的是为了进行参数验证
+    handleClickBtn(payload) {
+      if (typeof payload !== 'number') return false;
+      return true;
+    },
+  },
+
+  data() {
+    return {
+      addCounter: 2,
+    };
+  },
+
+  methods: {
+    decrement() {
+      this.$emit("handleClickBtn", -1);
+    },
+
+    increment() {
+      this.$emit("handleClickBtn", 1);
+    },
+
+    handleClickAddCounter() {
+      this.$emit("handleClickBtn", this.addCounter);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
