1ef0a15087fe4c14e59f768ddfc188ef48874e9d
pangxueming <1492678698@qq.com>
2023-02-01 18:40:02 +0800

    mitt事件总线的使用

---
+++ 03.learn_component/src/07.事件总线的使用/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="btnClick">btnClick</button>
+  </div>
+</template>
+
+<script>
+import mitter from "./utils/eventBus";
+
+export default {
+  methods: {
+    btnClick() {
+      mitter.emit('btnClickEvent1', {name: 'zhangsan', age: 25});
+      // mitter.emit('btnClickEvent2', {name: 'lisi', age: 26});
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ 03.learn_component/src/07.事件总线的使用/HomeCount.vue
@@ -0,0 +1,32 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import mitter from "./utils/eventBus";
+
+export default {
+  created() {
+    mitter.on("btnClickEvent1", this.btnClickEvent1Fn);
+
+    mitter.on("*", this.mitterAllFn);
+  },
+
+  methods: {
+    btnClickEvent1Fn(info) {
+      console.log(info);
+    },
+
+    mitterAllFn(type, info) {
+      console.log(type, info);
+    },
+  },
+
+  destroyed() {
+    mitter.all.clear();
+  },
+};
+</script>
+
+<style scoped>
+</style>
