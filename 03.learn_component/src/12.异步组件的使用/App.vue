1c2e76cb25e476c7baf8715342b1318d47c8a875
pangxueming <1492678698@qq.com>
2023-02-02 22:54:01 +0800

    异步组件的使用

---
+++ 03.learn_component/src/12.异步组件的使用/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>App组件</h2>
+    <home></home>
+    <async-category></async-category>
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+import Home from "./Home.vue";
+// import AsyncCategory from './AsyncCategory.vue';
+import Loading from './Loading.vue';
+
+// const AsyncCategory = defineAsyncComponent(() => import("./AsyncCategory.vue"));
+const AsyncCategory = defineAsyncComponent({
+  loader: () => import("./AsyncCategory.vue"),
+  loadingComponent: Loading,
+  // errorComponent: ErrorCpn
+  // delay： 在显示loadingComponent之前，等待多长时间
+  // delay: 2000 
+  /**
+   * err: 错误信息
+   * retry：一个函数，尝试retry尝试重新加载，使用（retry()）
+   * fail：一个函数，指示加载器结束退出
+   * attempts：记录最大尝试次数 
+   */
+  onError: function(err, retry, fail, attempts) {
+
+  }
+})
+
+export default {
+  components: {
+    Home,
+    AsyncCategory,
+  },
+};
+</script> 
--- 03.learn_component/src/main.js
+++ 03.learn_component/src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import './style/index.css'
-import App from './11.动态组件的使用/App.vue'
+import App from './12.异步组件的使用/App.vue'
+// import { sum } from './12.异步组件的使用/utils/match'
 
 const app = createApp(App)
 app.config.unwrapInjectedRef = true
 app.mount('#app')
+
+// console.log(sum(10, 20));
+// 通过import函数倒入的模块，后续webpack对其进行打包的时候就会进行分饱的操作
+// import('./12.异步组件的使用/utils/match').then(({ sum }) => { // 分包
+//   console.log(sum(10,20));
+// })
