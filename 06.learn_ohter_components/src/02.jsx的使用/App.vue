55bc451202a0c66ed2bf33a2365d78f83244681b
pangxueming <1492678698@qq.com>
2023-02-08 17:26:34 +0800

    jsx的使用

---
+++ 06.learn_ohter_components/src/02.jsx的使用/App.vue
@@ -0,0 +1,32 @@
+<script>
+import HelloWorld from './HelloWorld.vue';
+
+export default {
+  data() {
+    return {
+      counter: 1
+    }
+  },
+  render() {
+    const increment = () => {
+      this.counter++;
+    }
+
+    const decrement = () => {
+      this.counter--;
+    }
+
+    return (
+      <div>
+        <h2>当前计数：{this.counter}</h2>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+
+        <HelloWorld>
+          {{ default: (props) => (<button>我是按钮title: {props.title}</button>) }}
+        </HelloWorld>
+      </div>
+    )
+  }
+}
+</script>
---
+++ 06.learn_ohter_components/src/02.jsx的使用/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      title: 'hello world cpn'
+    }
+  },
+  render() {
+    return (
+      <div>
+        <h2>Hello world</h2>
+        {this.$slots.default ? this.$slots.default({title: this.title}) : (<span>哈哈哈</span>)}
+      </div>
+    );
+  }
+}
+</script>
