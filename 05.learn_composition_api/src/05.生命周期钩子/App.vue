e2c5835737a7fa2ceade82015c94e88b5f21637e
pangxueming <1492678698@qq.com>
2023-02-08 14:29:32 +0800

    生命周期钩子

---
+++ 05.learn_composition_api/src/05.生命周期钩子/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h2>当前计数： {{ counter }}</h2>
+    <button @click="counter++">+1</button>
+
+    <button @click="isShow = !isShow">toggle</button>
+    <template v-if="isShow">
+      <hello-world></hello-world>
+    </template>
+  </div>
+</template>
+
+<script>
+import { onMounted, onBeforeMount, onUpdated, onUnmounted, ref } from "vue";
+import HelloWorld from "./HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const counter = ref(0);
+    const isShow = ref(true);
+
+    onMounted(() => {
+      console.log("onMounted");
+    });
+
+    onBeforeMount(() => {
+      console.log("onBeforeMount");
+    });
+
+    onUpdated(() => {
+      console.log("onUpdated");
+    });
+
+    onUnmounted(() => {
+      console.log("onUnmounted");
+    });
+
+    return {
+      counter,
+      isShow
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ 05.learn_composition_api/src/05.生命周期钩子/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+export default {
+  setup() {
+    onMounted(() => {
+      console.log('hello world onMounted');
+    })
+  },
+};
+</script>
+
+<style scoped>
+</style>
