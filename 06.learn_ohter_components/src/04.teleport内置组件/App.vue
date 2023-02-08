255f02ca4810130be2be1ce0ca8c8f9366142f9e
pangxueming <1492678698@qq.com>
2023-02-08 17:27:04 +0800

    teleport内置组件

---
+++ 06.learn_ohter_components/src/04.teleport内置组件/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Teleport to="#new-app">
+      <h2>当前计数：{{ counter }}</h2>
+      <button @click="increment">+1</button>
+    </Teleport>
+
+    <Teleport to="#new-app">
+      <HelloWorld></HelloWorld>
+    </Teleport>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, getCurrentInstance } from 'vue';
+import HelloWorld from './HelloWorld.vue';
+
+const counter = ref(0);
+
+const instance = getCurrentInstance();
+console.log(instance.appContext.config.globalProperties.$description);
+// or
+// const {proxy} = getCurrentInstance();
+// console.log(proxy.$description);
+
+function increment() {
+  counter.value++;
+}
+
+onMounted(() => {
+  // console.log(proxy.$description);
+  console.log(instance.appContext.config.globalProperties.$description);
+}) 
+</script>
