ecc83864481b5fcd6ef9928ba368ff2d4d7405d1
pangxueming <1492678698@qq.com>
2023-02-08 14:29:57 +0800

    provide和inject

---
+++ 05.learn_composition_api/src/06.provide和inject/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <home />
+
+    <h2>app counter: {{ counter }}</h2>
+    <button @click="counter++">app +1</button>
+  </div>
+</template>
+
+<script>
+import { provide, ref, readonly } from "vue";
+import Home from "./Home.vue";
+
+export default {
+  components: {
+    Home,
+  },
+
+  setup() {
+    const name = ref("zhangsan");
+    const counter = ref(0);
+
+    provide("name", readonly(name));
+    provide("counter", readonly(counter));
+
+    return {
+      name,
+      counter,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ 05.learn_composition_api/src/06.provide和inject/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <h2>home当前计数：{{ counter }}</h2>
+
+    <button @click="counter++">home +1</button>
+  </div>
+</template>
+
+<script>
+import { inject } from "vue";
+export default {
+  setup() {
+    const name = inject("name");
+    const counter = inject("counter");
+
+    return {
+      name,
+      counter,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
